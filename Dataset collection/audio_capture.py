9d3c0758b65f16836b22fabb6bfbbda6fa758be8
Harshith D R <85189030+HarshithDR@users.noreply.github.com>
2022-11-12 17:41:31 +0530

    Delete audio_capture.py

--- Dataset collection/audio_capture.py
+++
@@ -1,9 +0,0 @@
-import sounddevice
-from scipy.io.wavfile import write
-
-fs = 44100
-seconds = 10
-
-aud = sounddevice.rec(int(seconds*fs),samplerate=fs,channels=1)
-sounddevice.wait()
-write('audio_out.wav',fs,aud)
